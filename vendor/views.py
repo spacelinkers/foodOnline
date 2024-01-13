ee9cea305ed82ac1283ef850b70f8040c3b8b221
spacelinkers <josen221@gmail.com>
2024-01-13 21:08:41 +0600

    extra edit.

--- vendor/views.py
+++ vendor/views.py
@@ -150,6 +150,7 @@ def add_food(request):
             print(form.errors)
     else:
         form = FoodItemForm()
+        form.fields['category'].queryset = Category.objects.filter(vendor=get_vendor(request))
     context = {
         'form': form,
     }
@@ -174,6 +175,7 @@ def edit_food(request, pk=None):
             print(form.errors)
     else:
         form = FoodItemForm(instance=food)
+        form.fields['category'].queryset = Category.objects.filter(vendor=get_vendor(request))
     context = {
         'form': form,
         'food': food,

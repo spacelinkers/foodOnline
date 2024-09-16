3baf2d20bd2ddb6e7941567c58dcd66ebd3b6831
spacelinkers <josen221@gmail.com>
2024-09-16 21:55:30 +0600

    customer profile updated-23

--- accounts/urls.py
+++ accounts/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('reset_password/', views.reset_password, name='reset_password'),
 
     path('vendor/', include('vendor.urls')),
+    path('customer/', include('customers.urls')),
 ]
